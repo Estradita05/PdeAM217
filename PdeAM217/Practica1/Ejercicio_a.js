9a70ac05cb73325d4d10caa32586a3c047f9911e
Estradita05 <124050385@upq.edu.mx>
2025-09-18 12:43:49 -0600

    Arregle el codigo con variables y arreglos con teemas vistos en clase

---
+++ PdeAM217/Practica1/Ejercicio_a.js
@@ -0,0 +1,10 @@
+const nombre = "Juan";   
+let edad = 20;           
+const pi = 3.1416;       
+
+edad = 25;
+
+console.log("=== Ejercicio A ===");
+console.log("Nombre:", nombre);
+console.log("Edad:", edad);
+console.log("Valor de PI:", pi);
